f0f14c04698112263db698b897554bc6f12fec4b
guoliim <showerdown2012@gmail.com>
2020-08-04 22:49:53 +0800

    add eslint & prettier

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ a react wrapper component for echarts which steal ideal from [echarts-for-react]
 ### To-do
 1. - [x] add types/index.d.ts support
 2. - [x] add eslint / typescript lint
-3. - [ ] add prettier
+3. - [x] add prettier
 4. - [ ] add test such jest, react-testing-library
 5. - [ ] add doc
-6. - [X] add example for local
+6. - [x] add example for local
 7. - [ ] add example for online
 8. - [ ] add publish script for npm registry & github pkg registry
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import equal from 'fast-deep-equal'
+import React from "react"
+import equal from "fast-deep-equal"
 
-import { addResizeListener, removeResizeListener } from './helper'
-import { EChartsOptionConfig, EChartOption, ECharts } from 'echarts'
+import { addResizeListener, removeResizeListener } from "./helper"
+import { EChartsOptionConfig, EChartOption, ECharts } from "echarts"
 
 export type OnType = {
-    event: string,
-    handler: (params: unknown) => unknown,
+    event: string
+    handler: (params: unknown) => unknown
 }
 
 export type DataType = {
-    option: Record<string, unknown>,
-    opts?: EChartsOptionConfig,
-    on?: OnType[],
+    option: Record<string, unknown>
+    opts?: EChartsOptionConfig
+    on?: OnType[]
 }
 
 export type InitialOptsType = {
-    devicePixelRatio?: number,
-    renderer?: string,
-    width?: number | string,
+    devicePixelRatio?: number
+    renderer?: string
+    width?: number | string
     height?: number | string
 }
 
 export type ChartProps = {
-    echarts: typeof echarts,
-    className?: string,
-    data: DataType,
-    opts?: InitialOptsType,
-    theme?: Record<string, unknown>,
-    disableLoading?: boolean,
-    onLoading?: (echarts: ECharts) => unknown,
-    onEchartsReady?: (echarts: ECharts) => unknown,
+    echarts: typeof echarts
+    className?: string
+    data: DataType
+    opts?: InitialOptsType
+    theme?: Record<string, unknown>
+    disableLoading?: boolean
+    onLoading?: (echarts: ECharts) => unknown
+    onEchartsReady?: (echarts: ECharts) => unknown
 }
 
 const Chart = ({
@@ -43,14 +43,14 @@ const Chart = ({
     onLoading,
     onEchartsReady,
 }: ChartProps) => {
-
     const isDidMount = React.useRef(false)
     const prevValue = React.useRef<
-        { 
-            data: DataType,
-            opts?: InitialOptsType,
-            theme?: Record<string, unknown>,
-        } | undefined
+        | {
+              data: DataType
+              opts?: InitialOptsType
+              theme?: Record<string, unknown>
+          }
+        | undefined
     >()
 
     const element = React.useRef<HTMLDivElement | null>(null)
@@ -62,47 +62,43 @@ const Chart = ({
         }
 
         if (element.current) {
-            chartElement.current = echarts.init(
-                element.current,
-                theme,
-                {
-                    ...{ renderer: 'svg' },
-                    ...opts,
-                }
-            )
+            chartElement.current = echarts.init(element.current, theme, {
+                ...{ renderer: "svg" },
+                ...opts,
+            })
         }
     }, [])
 
-    const setOptionHelper = React.useCallback((
-        option: EChartOption,
-        opts?: EChartsOptionConfig,
-        disableLoading?: boolean,
-        onLoading?: (echarts: ECharts) => unknown,
-    ) => {
-        if (!!chartElement.current) {
-            if (!!option) {
-                chartElement.current.setOption(
-                    option, 
-                    {
+    const setOptionHelper = React.useCallback(
+        (
+            option: EChartOption,
+            opts?: EChartsOptionConfig,
+            disableLoading?: boolean,
+            onLoading?: (echarts: ECharts) => unknown,
+        ) => {
+            if (!!chartElement.current) {
+                if (!!option) {
+                    chartElement.current.setOption(option, {
                         ...{ lazyUpdate: true },
                         ...opts,
+                    })
+
+                    if (disableLoading === true) {
+                        chartElement.current.hideLoading()
                     }
-                )
-                
-                if (disableLoading === true) {
-                    chartElement.current.hideLoading()
+
+                    if (disableLoading === false && disableLoading === undefined) {
+                        if (typeof onLoading === "function") {
+                            onLoading(chartElement.current)
+                        }
+                    }
                 }
 
-                if (disableLoading === false && disableLoading === undefined) {
-                    if (typeof onLoading === 'function') {
-                        onLoading(chartElement.current)
-                    }
-                }
+                return chartElement.current
             }
-
-            return chartElement.current
-        }
-    }, [])
+        },
+        [],
+    )
 
     const bindEventHandler = React.useCallback((on: OnType[]) => {
         if (on && on.length !== 0) {
@@ -114,20 +110,15 @@ const Chart = ({
 
     React.useEffect(() => {
         // did mount
-            
+
         initialEchartDom(opts, theme)
-        
+
         if (chartElement.current) {
-            data?.option && setOptionHelper(
-                data.option,
-                data.opts,
-                disableLoading,
-                onLoading,
-            )
+            data?.option && setOptionHelper(data.option, data.opts, disableLoading, onLoading)
             data?.on && bindEventHandler(data.on)
-            addResizeListener(element.current, () => (chartElement.current?.resize()))
-    
-            if (typeof onEchartsReady === 'function') {
+            addResizeListener(element.current, () => chartElement.current?.resize())
+
+            if (typeof onEchartsReady === "function") {
                 onEchartsReady(chartElement.current)
             }
         }
@@ -136,67 +127,54 @@ const Chart = ({
             // will un mount
 
             if (chartElement.current) {
-                removeResizeListener(element.current, () => (chartElement.current?.resize()))
-    
+                removeResizeListener(element.current, () => chartElement.current?.resize())
+
                 chartElement.current.dispose()
                 chartElement.current = null
                 prevValue.current = undefined
             }
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     React.useEffect(() => {
-        if (
-            !!chartElement.current &&
-            !!isDidMount.current &&
-            !!prevValue.current?.data && 
-            !!data
-        ) {
+        if (!!chartElement.current && !!isDidMount.current && !!prevValue.current?.data && !!data) {
             // did update
 
-            if (!equal(prevValue.current.data.option, data.option) 
-                && equal(prevValue.current.data.on, data.on)
-                && equal(prevValue.current.opts, opts)
+            if (
+                !equal(prevValue.current.data.option, data.option) &&
+                equal(prevValue.current.data.on, data.on) &&
+                equal(prevValue.current.opts, opts)
             ) {
                 chartElement.current.hideLoading()
-                setOptionHelper(
-                    data.option,
-                    data.opts,
-                    disableLoading,
-                    onLoading,
-                )
+                setOptionHelper(data.option, data.opts, disableLoading, onLoading)
 
-                if (typeof onEchartsReady === 'function') {
+                if (typeof onEchartsReady === "function") {
                     onEchartsReady(chartElement.current)
                 }
             }
 
             if (
-                !equal(prevValue.current.data.on, data.on) || 
+                !equal(prevValue.current.data.on, data.on) ||
                 !equal(prevValue.current.opts, opts) ||
                 !equal(prevValue.current.theme, theme)
-                ) {
+            ) {
                 initialEchartDom(opts, theme)
-                setOptionHelper(
-                    data.option,
-                    data.opts,
-                    disableLoading,
-                    onLoading,
-                )
+                setOptionHelper(data.option, data.opts, disableLoading, onLoading)
                 data?.on && bindEventHandler(data.on)
 
-                if (typeof onEchartsReady === 'function') {
+                if (typeof onEchartsReady === "function") {
                     onEchartsReady(chartElement.current)
                 }
             }
         }
     }, [
-        bindEventHandler, 
+        bindEventHandler,
         data,
         disableLoading,
         initialEchartDom,
-        onLoading, opts,
+        onLoading,
+        opts,
         setOptionHelper,
         theme,
         onEchartsReady,
@@ -214,9 +192,7 @@ const Chart = ({
         prevValue.current = { data, opts, theme }
     })
 
-    return (
-        <div className={`react-echarts-container ${className}`} ref={element} />
-    )
+    return <div className={`react-echarts-container ${className}`} ref={element} />
 }
 
-export default React.memo(Chart, (prevValue, nextValue) => (equal(prevValue, nextValue)))
+export default React.memo(Chart, (prevValue, nextValue) => equal(prevValue, nextValue))
